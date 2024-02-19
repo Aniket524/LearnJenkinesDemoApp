8618c26d320a506cfa323a3ec0c9086eed278146
Aniket524 <aniketkarp524@gmail.com>
2024-02-19 13:31:44 +0530

    added /hello route

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/demo',(req,res)=>{
     res.send('this is the demo repo')
 })
 
+app.get('/hello',(req,res)=>{
+    res.send('hello world')
+})
+
 app.listen(3001,()=>{
     console.log('app is listening on port 3001')
 })
